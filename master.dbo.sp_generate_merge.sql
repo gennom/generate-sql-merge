287b4a24363ce3050a1e89f69881f0bfa0dd37d7
gennom <gennom@outlook.com>
2014-12-03 23:36:37 +0300

    Update master.dbo.sp_generate_merge.sql
    RTRIM for uniqueidentifier type.

--- master.dbo.sp_generate_merge.sql
+++ master.dbo.sp_generate_merge.sql
@@ -361,7 +361,7 @@ WHILE @Column_ID IS NOT NULL
  'COALESCE('''''''' + RTRIM(CONVERT(char,' + @Column_Name + ',127))+'''''''',''NULL'')'
  WHEN @Data_Type IN ('uniqueidentifier') 
  THEN 
- 'COALESCE('''''''' + REPLACE(CONVERT(char(255),RTRIM(' + @Column_Name + ')),'''''''','''''''''''')+'''''''',''NULL'')'
+ 'COALESCE('''''''' + RTRIM(REPLACE(CONVERT(char(255),RTRIM(' + @Column_Name + ')),'''''''',''''''''''''))+'''''''',''NULL'')'
  WHEN @Data_Type IN ('text') 
  THEN 
  'COALESCE('''''''' + REPLACE(CONVERT(varchar(max),' + @Column_Name + '),'''''''','''''''''''')+'''''''',''NULL'')' 
